eee2995e8990df09460b28dabee9b1ca4696863a
sectionr0 <sectionr0@gmail.com>
2022-07-16 18:29:23 +0900

    jwt filter test
    jwt filter test

--- jwt/src/main/java/com/example/jwt/config/SecurityConfig.java
+++ jwt/src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.example.jwt.config;
 
+import com.example.jwt.filter.MyFilter1;
+import com.example.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -17,6 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class);
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 x
                 .and()
