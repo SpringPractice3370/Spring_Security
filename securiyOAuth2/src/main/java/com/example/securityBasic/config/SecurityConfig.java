619ebd439003db82c7a304289564d58f8dff9413
sectionr0 <sectionr0@gmail.com>
2022-07-19 23:51:14 +0900

    OAuth2UserService
    OAuth2UserService

--- securiyOAuth2/src/main/java/com/example/securityBasic/config/SecurityConfig.java
+++ securiyOAuth2/src/main/java/com/example/securityBasic/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.securityBasic.config;
 
+import com.example.securityBasic.config.oauth.PrincipleOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,10 +11,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+
+// 1. 코드 받기(인증) 2. 엑세스 토큰(코드를 통해 받음, 권한 생성)
+// 3. (권한을 통해서) 사용자 프로필 정보 가져옴. 4-1. 그 정보를 토대로 회원가입을 자동으로 진행시키기도 함
+// 4-2. (이메일, 전화번호, 이름, 아이디) 쇼핑몰 -> 집주소 더 필요 등등
+
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터 체인에 등록 됨.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, prePostEnabled : preAuthorize, postAuthorize 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PrincipleOauth2UserService principleOauth2UserService;
 
     // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해줌.
     @Bean
@@ -35,6 +45,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/")
                 .and()
                 .oauth2Login()
-                .loginPage("/loginForm"); // 구글 로그인이 완료된 뒤 후 처리가 필요
+                .loginPage("/loginForm") // 구글 로그인이 완료된 뒤 후 처리가 필요.
+                // Tip. 코드X, (엑세스 토큰 + 사용자 프로필 정보 O)
+                .userInfoEndpoint()
+                .userService(principleOauth2UserService);
     }
 }
