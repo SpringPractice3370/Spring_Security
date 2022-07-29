808f7ba4bf8edbfb186a82af2861b24144c668af
sectionr0 <sectionr0@gmail.com>
2022-07-29 22:04:23 +0900

    init
    init

---
+++ JwtWithOAuth2Test/src/main/java/com/example/JwtWithOAuth2Test/JwtWithOAuth2TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.JwtWithOAuth2Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtWithOAuth2TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtWithOAuth2TestApplication.class, args);
+	}
+
+}
