63f91e5cf1cd16ca4744080670f0720f318e7d3e
sectionr0 <sectionr0@gmail.com>
2022-08-02 05:33:30 +0900

    CorsProperties

--- JwtWithOAuth2Test/src/main/java/com/example/JwtWithOAuth2Test/JwtWithOAuth2TestApplication.java
+++ JwtWithOAuth2Test/src/main/java/com/example/JwtWithOAuth2Test/JwtWithOAuth2TestApplication.java
@@ -1,13 +1,15 @@
 package com.example.JwtWithOAuth2Test;
 
 import com.example.JwtWithOAuth2Test.config.properties.AppProperties;
+import com.example.JwtWithOAuth2Test.config.properties.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
-		AppProperties.class
+		AppProperties.class,
+		CorsProperties.class
 })
 public class JwtWithOAuth2TestApplication {
 
