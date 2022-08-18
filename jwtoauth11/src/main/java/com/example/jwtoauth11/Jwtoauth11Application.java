65071991f6957e70409294a1c07521ebfa79b3a8
sectionr0 <sectionr0@gmail.com>
2022-08-18 23:26:36 +0900

    init

---
+++ jwtoauth11/src/main/java/com/example/jwtoauth11/Jwtoauth11Application.java
@@ -0,0 +1,13 @@
+package com.example.jwtoauth11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwtoauth11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jwtoauth11Application.class, args);
+	}
+
+}
---
+++ jwtoauth11/src/test/java/com/example/jwtoauth11/Jwtoauth11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtoauth11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwtoauth11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
