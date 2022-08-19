bff70145b6e01ff2027dc998dbc580f8e0b1d194
sectionr0 <sectionr0@gmail.com>
2022-08-19 21:27:39 +0900

    sign controller

---
+++ jwtoauth11/src/main/java/com/example/jwtoauth11/controller/SignController.java
@@ -0,0 +1,35 @@
+package com.example.jwtoauth11.controller;
+
+import com.example.jwtoauth11.dto.MemberLoginRequestDto;
+import com.example.jwtoauth11.dto.MemberLoginResponseDto;
+import com.example.jwtoauth11.dto.MemberRegisterRequestDto;
+import com.example.jwtoauth11.dto.MemberRegisterResponseDto;
+import com.example.jwtoauth11.dto.result.SingleResult;
+import com.example.jwtoauth11.service.ResponseService;
+import com.example.jwtoauth11.service.SignService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/sign")
+public class SignController {
+
+    private final SignService signService;
+    private final ResponseService responseService;
+
+    @PostMapping("/register")
+    public SingleResult<MemberRegisterResponseDto> register(@RequestBody MemberRegisterRequestDto requestDto) {
+        MemberRegisterResponseDto responseDto = signService.registerMember(requestDto);
+        return responseService.getSingleResult(responseDto);
+    }
+
+    @PostMapping("/login")
+    public SingleResult<MemberLoginResponseDto> login(@RequestBody MemberLoginRequestDto requestDto) {
+        MemberLoginResponseDto responseDto = signService.loginMember(requestDto);
+        return responseService.getSingleResult(responseDto);
+    }
+}
