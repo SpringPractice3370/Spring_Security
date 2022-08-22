25dfcd757ef57efd49355f5e8abcdfe4c7a50d10
sectionr0 <sectionr0@gmail.com>
2022-08-22 23:43:54 +0900

    oauth2 filter add

--- JwtWithOAuth2Test-Credential/src/main/java/com/example/JwtWithOAuth2TestCredential/security/config/SecurityConfig.java
+++ JwtWithOAuth2Test-Credential/src/main/java/com/example/JwtWithOAuth2TestCredential/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.JwtWithOAuth2TestCredential.security.config;
 
+import com.example.JwtWithOAuth2TestCredential.security.oauth.filter.OAuth2AccessTokenAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,8 +11,18 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final OAuth2AccessTokenAuthenticationFilter oAuth2AccessTokenAuthenticationFilter;
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/login/oauth2/*").permitAll() //로그인 화면 접근 가능
+                .antMatchers("/").permitAll() //메인 화면 접근 가능
+                .anyRequest().hasRole("USER");
+
+        http.addFilterBefore(oAuth2AccessTokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     }
 
