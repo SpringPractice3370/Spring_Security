fdce733a5bfbe948ed4fa16c2cbb9067a6d1ace6
sectionr0 <sectionr0@gmail.com>
2022-08-22 23:28:24 +0900

    initial

---
+++ JwtWithOAuth2Test-Credential/src/main/java/com/example/JwtWithOAuth2TestCredential/JwtWithOAuth2TestCredentialApplication.java
@@ -0,0 +1,13 @@
+package com.example.JwtWithOAuth2TestCredential;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtWithOAuth2TestCredentialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtWithOAuth2TestCredentialApplication.class, args);
+	}
+
+}
---
+++ JwtWithOAuth2Test-Credential/src/test/java/com/example/JwtWithOAuth2TestCredential/JwtWithOAuth2TestCredentialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JwtWithOAuth2TestCredential;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtWithOAuth2TestCredentialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
