1bfdb1f0829c13f7090fee6080784ac3ae2e5f2d
sectionr0 <sectionr0@gmail.com>
2022-08-22 23:42:45 +0900

    Social type service

---
+++ JwtWithOAuth2Test-Credential/src/main/java/com/example/JwtWithOAuth2TestCredential/security/oauth/service/LoadUserService.java
@@ -0,0 +1,48 @@
+package com.example.JwtWithOAuth2TestCredential.security.oauth.service;
+
+import com.example.JwtWithOAuth2TestCredential.domain.SocialType;
+import com.example.JwtWithOAuth2TestCredential.security.oauth.authentication.AccessTokenSocialTypeToken;
+import com.example.JwtWithOAuth2TestCredential.security.oauth.authentication.OAuth2UserDetails;
+import com.example.JwtWithOAuth2TestCredential.security.oauth.service.strategy.GoogleLoadStrategy;
+import com.example.JwtWithOAuth2TestCredential.security.oauth.service.strategy.KakaoLoadStrategy;
+import com.example.JwtWithOAuth2TestCredential.security.oauth.service.strategy.NaverLoadStrategy;
+import com.example.JwtWithOAuth2TestCredential.security.oauth.service.strategy.SocialLoadStrategy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class LoadUserService {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private SocialLoadStrategy socialLoadStrategy;//추상 클래스, 로그인을 진행하는 사이트레 따라 달라짐
+
+
+    public OAuth2UserDetails getOAuth2UserDetails(AccessTokenSocialTypeToken authentication)  {
+
+        SocialType socialType = authentication.getSocialType();
+
+        setSocialLoadStrategy(socialType);//SocialLoadStrategy 설정
+
+        String socialPk = socialLoadStrategy.getSocialPk(authentication.getAccessToken());//PK 가져오기
+
+        return OAuth2UserDetails.builder() //PK와 SocialType을 통해 회원 생성
+                .socialId(socialPk)
+                .socialType(socialType)
+                .build();
+    }
+
+    private void setSocialLoadStrategy(SocialType socialType) {
+        this.socialLoadStrategy = switch (socialType){
+
+            case KAKAO -> new KakaoLoadStrategy();
+            case GOOGLE ->  new GoogleLoadStrategy();
+            case NAVER ->  new NaverLoadStrategy();
+            default -> throw new IllegalArgumentException("지원하지 않는 로그인 형식입니다");
+        };
+    }
+
+
+}
---
+++ JwtWithOAuth2Test-Credential/src/main/java/com/example/JwtWithOAuth2TestCredential/security/oauth/service/strategy/GoogleLoadStrategy.java
@@ -0,0 +1,29 @@
+package com.example.JwtWithOAuth2TestCredential.security.oauth.service.strategy;
+
+import com.example.JwtWithOAuth2TestCredential.domain.SocialType;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+@Slf4j
+public class GoogleLoadStrategy extends SocialLoadStrategy{
+
+
+
+    protected String sendRequestToSocialSite(HttpEntity request){
+        try {
+            ResponseEntity<Map<String, Object>> response = restTemplate.exchange(SocialType.GOOGLE.getUserInfoUrl(),
+                    SocialType.GOOGLE.getMethod(),
+                    request,
+                    RESPONSE_TYPE);
+
+            return (response.getBody().get("email")).toString();//구글은 email를 PK로 사용
+
+        } catch (Exception e) {
+            log.error("AccessToken을 사용하여 GOOGLE 유저정보를 받아오던 중 예외가 발생했습니다 {}" ,e.getMessage() );
+            throw e;
+        }
+    }
+}
