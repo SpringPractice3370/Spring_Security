8ce664e41339b77b8358cdbaabd7c227eeb6737d
sectionr0 <sectionr0@gmail.com>
2022-11-20 15:50:48 +0900

    init commit

---
+++ kotlinWithSecurityJWT/src/main/kotlin/com/springpratice3370/kotlinWithSecurityJWT/KotlinWithSecurityJwtApplication.kt
@@ -0,0 +1,11 @@
+package com.springpratice3370.kotlinWithSecurityJWT
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinWithSecurityJwtApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinWithSecurityJwtApplication>(*args)
+}
---
+++ kotlinWithSecurityJWT/src/test/kotlin/com/springpratice3370/kotlinWithSecurityJWT/KotlinWithSecurityJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springpratice3370.kotlinWithSecurityJWT
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinWithSecurityJwtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
